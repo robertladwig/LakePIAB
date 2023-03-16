b444520428860db564f554ad67bc51d3001d650d
robertladwig <ladwigjena@gmail.com>
2023-03-15 20:54:47 -0500

    newest finetuning

--- src/run_HybridModel.py
+++ src/run_HybridModel.py
@@ -94,13 +94,14 @@ meteo_all = provide_meteorology(meteofile = '../input/Mendota_2002.csv',
                     windfactor = 1.0)
                      
 hydrodynamic_timestep = 24 * dt
-total_runtime =  365 * hydrodynamic_timestep/dt  #365 *1 # 14 * 365
+total_runtime =  3 * hydrodynamic_timestep/dt  #365 *1 # 14 * 365
 startTime =   (0 + 365*10) * hydrodynamic_timestep/dt #150 * 24 * 3600
 endTime =  (startTime + total_runtime) # * hydrodynamic_timestep/dt) - 1
 
 startingDate = meteo_all[0]['date'][startTime] #* hydrodynamic_timestep/dt]
 endingDate = meteo_all[0]['date'][(endTime - 1)]#[(startTime + total_runtime)]# * hydrodynamic_timestep/dt -1]
 # endingDate = meteo_all[0]['date'][(startTime + total_runtime * hydrodynamic_timestep/dt) - 1]
+
 
 #26280
 times = pd.date_range(startingDate, endingDate, freq='H')
